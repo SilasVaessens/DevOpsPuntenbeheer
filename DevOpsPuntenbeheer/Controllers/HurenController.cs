661068e8f4cd4a4de9181987dcfdbd9d5a0d4b07
Silas Vaessens <1820435vaessens@zuyd.nl>
2021-05-27 18:36:38 +0200

    Add project files.

---
+++ DevOpsPuntenbeheer/Controllers/HurenController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevOpsPuntenbeheer.Model;
+
+namespace DevOpsPuntenbeheer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HurenController : Controller
+    {
+        private static Huren test = new Huren();
+
+        public IActionResult Get()
+        {
+            return Ok(test);
+        }
+
+    }
+}
---
+++ DevOpsPuntenbeheer/Model/Huren.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevOpsPuntenbeheer.Model
+{
+    public class Huren
+    {
+        public int ID { get; set; }
+        public string Woord { get; set; } = "Test";
+
+    }
+}
